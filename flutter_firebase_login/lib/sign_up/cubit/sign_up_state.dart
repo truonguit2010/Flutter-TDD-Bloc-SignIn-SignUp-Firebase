3af05c39c8ae6442cde5e583a0fec908a36e5527
Pham Son Truong <truongps@sendo.vn>
2021-01-08 15:56:33 +0700

    - Done implement sign_up cubit.

--- flutter_firebase_login/lib/sign_up/cubit/sign_up_state.dart
+++ flutter_firebase_login/lib/sign_up/cubit/sign_up_state.dart
@@ -1,6 +1,32 @@
 part of 'sign_up_cubit.dart';
 
-@immutable
-abstract class SignUpState {}
+enum ConfirmPasswordValidationError { invalid }
 
-class SignUpInitial extends SignUpState {}
+class SignUpState extends Equatable {
+
+  final Email email;
+  final Password password;
+  final ConfirmedPassword confirmedPassword;
+  final FormzStatus status;
+
+  const SignUpState({
+    this.email = const Email.pure(),
+    this.password = const Password.pure(),
+    this.confirmedPassword = const ConfirmedPassword.pure(),
+    this.status = FormzStatus.pure
+  });
+
+  @override
+  List<Object> get props => [email, password, confirmedPassword, status];
+
+  SignUpState copyWith({Email email, Password password, ConfirmedPassword confirmedPassword, FormzStatus status}) {
+
+    return SignUpState(
+      email: email ?? this.email,
+      password: password ?? this.password,
+      confirmedPassword: confirmedPassword ?? this.confirmedPassword,
+      status: status ?? this.status
+    );
+  }
+}
+
