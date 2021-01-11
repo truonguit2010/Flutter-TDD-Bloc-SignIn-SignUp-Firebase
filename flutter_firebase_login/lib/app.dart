bf9075fcdb7a2763c879974185e9ae057797d601
Pham Son Truong <truongps@sendo.vn>
2021-01-11 16:48:28 +0700

    - Build simple bloc and connect some parts of the app.

---
+++ flutter_firebase_login/lib/app.dart
@@ -0,0 +1,71 @@
+
+
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_firebase_login/authentication/authentication.dart';
+import 'package:flutter_firebase_login/login/view/login_page.dart';
+import 'package:flutter_firebase_login/theme.dart';
+import 'package:flutter_firebase_login/authentication/authentication.dart';
+
+class App extends StatelessWidget {
+
+  final AuthenticationRepository authenticationRepository;
+
+  App({Key key, @required this.authenticationRepository}) :
+        assert(authenticationRepository != null),
+        super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+        value: authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AuthenticationBloc(authenticationRepository: authenticationRepository,),
+        child: null,
+      ),
+    );
+  }
+}
+
+class AppView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return null;
+  }
+}
+
+class _AppViewState extends State<AppView> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+
+  NavigatorState get _navigator => _navigatorKey.currentState;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      navigatorKey: _navigatorKey,
+      builder: (context, child) {
+        return BlocListener<AuthenticationBloc, AuthenticationState>(
+          listener: (context, state) {
+            switch (state.status) {
+              case AuthenticationStatus.authenticated:
+//                _navigator.pushAndRemoveUntil(newRoute, (route) => false)
+                break;
+              case AuthenticationStatus.unauthenticated:
+                _navigator.pushAndRemoveUntil(LoginPage.route(), (route) => false);
+                break;
+              default:
+                break;
+            }
+          },
+          child: child,
+        );
+      },
+    );
+  }
+}
